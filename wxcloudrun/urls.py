088afe6cdfb604ab99dfc1388ed5b4bdf3407885
SolisRexD <116064479+SolisRexD@users.noreply.github.com>
2024-06-05 22:20:28 +0800

    url和views得到了更改，增加了SimpleLoginView，作为简单的登录

--- wxcloudrun/urls.py
+++ wxcloudrun/urls.py
@@ -17,10 +17,15 @@ Including another URLconf
 from wxcloudrun import views
 from django.conf.urls import url
 
+from django.urls import path
+from views import SimpleLoginView
+
 urlpatterns = (
     # 计数器接口
     url(r'^^api/count(/)?$', views.counter),
 
     # 获取主页
     url(r'(/)?$', views.index),
+    path('login/', SimpleLoginView.as_view(), name='simple_login'),
+
 )
